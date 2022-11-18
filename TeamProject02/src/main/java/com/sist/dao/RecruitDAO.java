c63bc48e1127c97348f90f0506bbaa07787389c6
LHH1115 <hohyung95@gamil.com>
2022-11-18 09:53:05 +0900

    first commit

---
+++ TeamProject02/src/main/java/com/sist/dao/RecruitDAO.java
@@ -0,0 +1,30 @@
+package com.sist.dao;
+
+import com.sist.vo.RecruitVO;
+
+public class RecruitDAO {
+	public int submitResume() {
+		int re = -1;
+		
+		return re;
+	}
+	
+	public RecruitVO findMyResume() {
+		RecruitVO r = new RecruitVO();
+		
+		return r;
+	}
+	
+	public int UpdateResume() {
+		int re = -1;
+		
+		return re;
+	}
+	
+	public int setSchedule() {
+		int re = -1;
+		
+		return re;
+	}
+	
+}
