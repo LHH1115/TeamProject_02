fb0180205254c6e04af721964cf9ff3b7e6c1250
Rinkle0930 <junghwa0930@naver.comm>
2022-11-21 09:33:52 +0900

    XX

--- TeamProject02/src/main/java/com/sist/action/NewOrderOKAction.java
+++ TeamProject02/src/main/java/com/sist/action/NewOrderOKAction.java
@@ -6,12 +6,28 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.sist.dao.OrderDAO;
+import com.sist.vo.OrderVO;
+
 public class NewOrderOKAction implements CompanyAction {
 
 	@Override
 	public String pro(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		return null;
+		OrderVO o = new OrderVO();
+		o.setCName(request.getParameter("cName"));
+		o.setCPhone(request.getParameter("cPhone"));
+		o.setCManager(request.getParameter("cManager"));
+		o.setCEmail(request.getParameter("cEmail"));
+		o.setCAddr(request.getParameter("cAddr"));
+		o.setCService(Integer.parseInt(request.getParameter("cService")));
+		o.setCPrice(Integer.parseInt(request.getParameter("cPrice")));
+		o.setCPortfolio(request.getParameter("cPortfolio"));
+		o.setCInfo(request.getParameter("cInfo"));
+		
+		OrderDAO dao = OrderDAO.getInstance();
+		int re = dao.insertNewOrder(o);
+		request.setAttribute("re", re);
+		return "newOrderOK.jsp";
 	}
 
 }
