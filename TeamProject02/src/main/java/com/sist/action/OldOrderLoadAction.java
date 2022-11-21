6e9d6661998b9d4937cfde3250cdcf540aa72266
LHH1115 <hohyung95@gmail.com>
2022-11-21 11:56:45 +0900

    수정

--- TeamProject02/src/main/java/com/sist/action/OldOrderLoadAction.java
+++ TeamProject02/src/main/java/com/sist/action/OldOrderLoadAction.java
@@ -1,5 +1,15 @@
 package com.sist.action;
 
-public class OldOrderLoadAction {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class OldOrderLoadAction implements CompanyAction {
+	@Override
+	public String pro(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
